fa0ed12ad7bc697eccc1d9359636ba504bfe77ec
Ivan Olson <ivan@olsonish.com>
2012-11-14 16:53:05 -0800

    header/footer in reasonably happy place FF/Chrome

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,28 @@
+var headerCtx = $('header')[0];
+var footerCtx = $('footer')[0];
 
+
+/* Header Search */
+    /* Context */
+    var headerSearchBox = $('.search-text', headerCtx);
+    var headerSearchSubmit = $('.search-submit', headerCtx);
+    /* Params */
+    var hSBDefaultText = headerSearchBox.attr("value");
+    /* Cursor goes in the Box */
+    headerSearchBox.focus(function (e) {
+        $(this).addClass("active");
+        if ($(this).attr("value") == hSBDefaultText) { $(this).attr("value", ""); }
+    });
+    headerSearchBox.blur(function (e) {
+        $(this).removeClass("active");
+        if ($(this).attr("value") == "") { $(this).attr("value", hSBDefaultText); }
+    });
+    /* search button gets clicked */
+    headerSearchSubmit.click(function () {
+        if (headerSearchBox.attr("value") == hSBDefaultText) {
+            alert('Search clicked but value is default');
+        } else {
+            alert('Search for ' + headerSearchBox.attr("value"));
+        }
+    });
+/* Header Search */
