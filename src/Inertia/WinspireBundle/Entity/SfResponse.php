8cf2b4db4f580b7fad7ea314dbc41e6a33193d91
Douglas Choma <doug@inertiaim.com>
2013-03-28 00:10:31 -0700

    Rename SfResponse

---
+++ src/Inertia/WinspireBundle/Entity/SfResponse.php
@@ -0,0 +1,13 @@
+<?php
+namespace Inertia\WinspireBundle\Entity;
+
+use BeSimple\SoapBundle\ServiceDefinition\Annotation as Soap;
+
+class SfResponse
+{
+    /**
+     * @Soap\ComplexType("boolean")
+     */
+    public $Ack;
+    
+}
