df2c37077caebcc9284e1b0d834bde5f2553d7d2
Douglas Choma <doug@inertiaim.com>
2013-03-31 15:28:36 -0700

    Add response header to reveal download file size

--- src/Inertia/WinspireBundle/Controller/SuitcaseController.php
+++ src/Inertia/WinspireBundle/Controller/SuitcaseController.php
@@ -253,8 +253,10 @@ class SuitcaseController extends Controller
         $zip->close();
         
         $response = new Response();
-        $response->headers->set('Content-Type', 'application/zip');
         $response->headers->set('Content-Disposition', 'attachment; filename="' . 'Winspire-Materials.zip' . '"');
+        $response->headers->set('Content-Type', 'application/zip');
+        $response->headers->set('Content-Description: File Transfer');
+        $response->headers->set('Content-Length: ' . filesize($filename));
         
         return $response->setContent(file_get_contents($filename));
     }
