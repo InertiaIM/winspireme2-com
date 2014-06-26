196980d33346405b57fac4570fc67b29f4fbac08
Douglas Choma <doug@inertiaim.com>
2014-06-26 14:27:07 -0700

    Fix for new Partner sync to whitelabel model

--- src/Inertia/WinspireBundle/Services/AccountSoapService.php
+++ src/Inertia/WinspireBundle/Services/AccountSoapService.php
@@ -254,12 +254,9 @@ class AccountSoapService
                 
                 if (!$partner) {
                     // New partner, not in our database yet
-                    if ((isset($a->White_Label_user__c) && $a->White_Label_user__c == '1')) {
-                        $this->logger->info('New partner (' . $id . ') to be added');
-                        $partner = new Partner();
-                        $partner->setCreated($a->CreatedDate);
-//                        $partner->setActive(true);
-                    }
+                    $partner = new Partner();
+                    $partner->setCreated($a->CreatedDate);
+                    $this->logger->info('New partner (' . $id . ') to be added');
                 } else {
                     // Partner already exists, just update
                     $this->logger->info('Existing partner (' . $id . ') to be updated');
